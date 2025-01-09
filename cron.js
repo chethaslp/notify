4089160fefbcb00d543b7803e34c2f06a1094bfb
chethas.lp <lchethas@gmail.com>
2025-01-09 18:49:12 +0530

    FIX: site's unreachability failing cron jobs

--- cron.js
+++ cron.js
@@ -29,75 +29,78 @@ async function cron() {
   }else console.log(`Checking ${sites.data.length} sites.`)
 
   sites.data.forEach(async site => {
+    try {
+      const latestSiteData = await axios.get(site.url);
 
-    const latestSiteData = await axios.get(site.url);
-
-    if(!site.data) {
-      await supabase.from('sites').update({data: latestSiteData.data, last_checked: new Date()}).match({url: site.url});
-      console.log(site.url + ": Initial data added.") ;
-    }else {
-      const latestParsed = parse(latestSiteData.data);
-      const cacheParsed = parse(site.data);
-      const res = [];
-      let itt = false;
-
-      latestParsed.querySelectorAll('tr.displayList').forEach(e => {
-        itt = false;
-        cacheParsed.querySelectorAll('tr.displayList').forEach(c => {
-          e.querySelector('td:nth-child(2)').textContent.trim() == c.querySelector('td:nth-child(2)').textContent.trim() ? itt =true : null;
-        })
-        if(!itt) res.push(e);
-    });
-
-      if(res.length > 0) {
-        console.log(site.url + ": Content Changed.");
+      if(!site.data) {
         await supabase.from('sites').update({data: latestSiteData.data, last_checked: new Date()}).match({url: site.url});
+        console.log(site.url + ": Initial data added.") ;
+      }else {
+        const latestParsed = parse(latestSiteData.data);
+        const cacheParsed = parse(site.data);
+        const res = [];
+        let itt = false;
 
-        res.forEach(async ch => {
-          let title = ch.querySelector('td:nth-child(2)').textContent.trim();
-          let att = ch.querySelector('a').getAttribute('href');
-          
-          // Check if the title contains the name of the college for filtering out unnecessary updates
-          const normalizedString = title.toLowerCase()
-            .replace(/[\n\r]+/g, ' ')
-            .replace(/\s+/g, ' ')
-            .trim();
-          
-          if(/b.tech/.test(normalizedString)){
-            if(!(/2020 scheme/.test(normalizedString))) {
-              // If the title does not contain the 2020 scheme, check it's attachment for the keyword
-              console.log(site.url + ": Checking attachment : " + att);
-              const pdfText = await readPdfText({url: att});
-              if (!(/2020 scheme/.test(pdfText.toLowerCase()) && /b.tech/.test(pdfText.toLowerCase()))) {
-                return
+        latestParsed.querySelectorAll('tr.displayList').forEach(e => {
+          itt = false;
+          cacheParsed.querySelectorAll('tr.displayList').forEach(c => {
+            e.querySelector('td:nth-child(2)').textContent.trim() == c.querySelector('td:nth-child(2)').textContent.trim() ? itt =true : null;
+          })
+          if(!itt) res.push(e);
+      });
+
+        if(res.length > 0) {
+          console.log(site.url + ": Content Changed.");
+          await supabase.from('sites').update({data: latestSiteData.data, last_checked: new Date()}).match({url: site.url});
+
+          res.forEach(async ch => {
+            let title = ch.querySelector('td:nth-child(2)').textContent.trim();
+            let att = ch.querySelector('a').getAttribute('href');
+            
+            // Check if the title contains the name of the college for filtering out unnecessary updates
+            const normalizedString = title.toLowerCase()
+              .replace(/[\n\r]+/g, ' ')
+              .replace(/\s+/g, ' ')
+              .trim();
+            
+            if(/b.tech/.test(normalizedString)){
+              if(!(/2020 scheme/.test(normalizedString))) {
+                // If the title does not contain the 2020 scheme, check it's attachment for the keyword
+                console.log(site.url + ": Checking attachment : " + att);
+                const pdfText = await readPdfText({url: att});
+                if (!(/2020 scheme/.test(pdfText.toLowerCase()) && /b.tech/.test(pdfText.toLowerCase()))) {
+                  return
+                }
               }
+            }else return;
+
+            // Check if the attachment is a valid link
+            if(!att || !att.startsWith('https://exams.keralauniversity.ac.in')) att = null;
+
+            const mailOptions = {
+              from: 'Notify',
+              to: process.env.TO_EMAIL,
+              subject: `New Update from ${site.name}!`,
+              text: 'A new update is available for the site: '+ site.url + '\n' + res.join("\n"),
+              html: template(title, att, site.url, site.name)
             }
-          }else return;
 
-          // Check if the attachment is a valid link
-          if(!att || !att.startsWith('https://exams.keralauniversity.ac.in')) att = null;
-
-          const mailOptions = {
-            from: 'Notify',
-            to: process.env.TO_EMAIL,
-            subject: `New Update from ${site.name}!`,
-            text: 'A new update is available for the site: '+ site.url + '\n' + res.join("\n"),
-            html: template(title, att, site.url, site.name)
-          }
-
-          console.log(site.url + ": New Update : " + title);
-          tr.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log("Email NOT sent: " + error.code);
-            } else {
-              console.log(site.url + ': Email sent [CHANGED] to ' + mailOptions.to);
-            }
+            console.log(site.url + ": New Update : " + title);
+            tr.sendMail(mailOptions, function(error, info){
+              if (error) {
+                console.log("Email NOT sent: " + error.code);
+              } else {
+                console.log(site.url + ': Email sent [CHANGED] to ' + mailOptions.to);
+              }
+            });
           });
-        });
-      }else{
-        console.log(site.url + ": No Content Changes. ")
+        }else{
+          console.log(site.url + ": No Content Changes. ")
+        }
       }
-    }
+  } catch (error) {
+    console.log(site.url + "Site Unreachable. ");
+  }
   });
 }
 
